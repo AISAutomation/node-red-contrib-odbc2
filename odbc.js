b6c6f6cafefc43525310a5df1179973724c23a38
Ryan Cronin <ryancronin23@gmail.com>
2019-07-03 08:59:11 -0400

    Initial Commit

--- odbc.js
+++ odbc.js
@@ -1,31 +1,13 @@
 module.exports = function(RED) {
-	"use strict";
-	var mustache = require("mustache");
-	var Pool = require("odbc").Pool
+	const mustache = require("mustache");
+	const odbc = require('odbc');
 
 	function connection(config) {
 	    RED.nodes.createNode(this, config);
-	    this.cn = "";
-	    if (config.driver!=""){
-	    	this.cn = this.cn + "DRIVER="+config.driver + ";"
-	    }
-	    if (config.server!=""){
-	    	this.cn = this.cn + "SERVER="+config.server + ";"
-	    }
-	    if (this.credentials.username!=""){
-	    	this.cn = this.cn + "USER="+ this.credentials.username + ";"
-	    }
-   	    if (this.credentials.password != ""){
-	    	this.cn = this.cn + "PASSWORD="+ this.credentials.password + ";"
-	    }
-	    this.cn = this.cn + config.other;
-	    this.pool = new Pool();
-	    var node = this;
-/*	   	node.on('close',function(){
-	   		node.pool.close(function(){});
-	   	})
-*/	}
-  	RED.nodes.registerType("ODBC-CN", connection, {
+		this.connection = `${config.connection};UID=${confi.username};PWD=${config.password}`;
+	}
+	
+  	RED.nodes.registerType("ODBC_CONNECTION", connection, {
 	    credentials: {
 			username: {type:"text"},
 			password: {type:"password"}
@@ -33,65 +15,43 @@ module.exports = function(RED) {
 	});
 
   	function odbc(config) {
-	    RED.nodes.createNode(this, config);
-	    var odbcCN = RED.nodes.getNode(config.odbcCN);
-	    this.query = config.query;
-	    this.pool = odbcCN.pool;
-	    this.cn = odbcCN.cn;
-	    this.outField = config.outField;
-	    var node = this;
-	    var b = node.outField.split(".");
-        var i = 0;
-        var r = null;
-        var m = null;
-        var rec = function(obj) {
-            i += 1;
-            if ((i < b.length) && (typeof obj[b[i-1]] === "object")) {
-                rec(obj[b[i-1]]); // not there yet - carry on digging
-            }
-            else {
-                 if (i === b.length) { // we've finished so assign the value
-                     obj[b[i-1]] = r;
-                     node.send(m);
-                     node.status({});
-                 }
-                 else {
-                     obj[b[i-1]] = {}; // needs to be a new object so create it
-                     rec(obj[b[i-1]]); // and carry on digging
-                 }
-            }
-        }
-/*		node.on('input',function(msg){
-			node.pool.open(node.cn, function(err, db){
-		    	if (err){
-		    		node.error(err);
-		    		node.status({fill:"red",shape:"ring",text:"Error"});
-		    		return;
-		    	}*/
+		RED.nodes.createNode(this, config);
+		const node = this;
+		node.connection = RED.nodes.getNode(config.odbc);
+		node.query = config.query;
+		node.outfield = config.outField;
 
-		node.pool.open(node.cn, function(err, db){
-        	if (err) {
-	    		node.error(err);
-	    		node.status({fill:"red",shape:"ring",text:"Error"});
-	    		db.close(function(){});
-	    		return;
-	    	}
-			node.on('input', function(msg) {
-		    	node.status({fill:"blue",shape:"dot",text:"requesting"});
-		    	var query = mustache.render(node.query,msg);
-		    	db.query(query, function (err, rows, moreResultSets){
-	        		if (err) {
-	        			node.error(err);
-	        			node.status({fill:"red",shape:"ring",text:"Error"});
-	        			db.close(function(){});
-	        			return;
-	        		}
-	        		i = 0;
-	        		r = rows;
-	        		m = msg;
-	        		rec(msg);
-	        	});
-		    });
+		odbc.pool(node.connection, (error, pool) => {
+			if (error) {
+				node.error(error);
+				node.status({fill: "red", shape: "ring", text: error.message});
+			}
+
+			pool.connect((error, connection) => {
+				if (error) {
+					node.error(error);
+					node.status({fill: "red", shape: "ring", text: error.message});
+				}
+
+				node.on('input', (msg) => {
+					node.status({fill:"blue",shape:"dot",text:"requesting"});
+					const query = mustache.render(node.query, msg);
+	
+					pool.query(query, (error, results) => {
+						if (error) {
+							node.error(error);
+							node.status({fill: "red", shape: "ring", text: error.message});
+							pool.close();
+							return;
+						}
+
+						msg.payload = results;
+
+						node.send(msg);
+						node.status({fill:'blue',shape:'dot',text:'finish'});
+					})
+				});
+			});
 		});
 	}
   	RED.nodes.registerType("ODBC", odbc);
